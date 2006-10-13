75f9b5804d23c8654d564405b2a69ebb2d7703b0
Ted Serbinski <m3avrck@12932.no-reply.drupal.org>
2006-10-13 17:26:49 +0000

    various code cleanups, consistent use of ';'

--- node.tpl.php
+++ node.tpl.php
@@ -1,27 +1,27 @@
 <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>" id="node-<?php print $node->nid; ?>">
   <?php if ($page == 0): ?>
     <h2 class="title">
-      <a href="<?php print $node_url ?>"><?php print $title ?></a>
+      <a href="<?php print $node_url ?>"><?php print $title; ?></a>
     </h2>
   <?php endif; ?>
 
-  <?php if ($picture) print $picture ?>  
+  <?php if ($picture) print $picture; ?>  
   
   <?php if ($submitted): ?>
     <span class="submitted"><?php print t('Posted ') . format_date($node->created, 'custom', "F jS, Y") . t(' by ') . theme('username', $node); ?></span> 
   <?php endif; ?>
   
   <div class="content">
-    <?php print $content?>
+    <?php print $content; ?>
   </div>
   
   <?php if ($links): ?>
     <div class="links">
-      <?php print $links ?>
+      <?php print $links; ?>
     </div>
   <?php endif; ?>
   
   <?php if (count(taxonomy_node_get_terms($node->nid))): ?>
-    <div class="taxonomy"><?php print t('tags: ') . $terms ?></div>
+    <div class="taxonomy"><?php print t('tags: ') . $terms; ?></div>
   <?php endif; ?>     
 </div>
--- page.tpl.php
+++ page.tpl.php
@@ -2,59 +2,60 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language ?>" xml:lang="<?php print $language ?>">
 
 <head>
-  <title><?php print $head_title ?></title>
-  <?php print $head ?>
-  <?php print $styles ?>
-  <?php print $scripts ?>
-  </head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
 
 <body id="<?php if ($is_front) { print 'home'; } else { print 'second'; } ?>">
   <div id="page">
     <div id="header">
       <div id="logo-title">
       
-      <?php print $search_box ?>      
+        <?php print $search_box; ?>      
         <?php if ($logo): ?>
-          <a href="<?php print $base_path ?>" title="<?php print t('Home') ?>">
-            <img src="<?php print $logo ?>" alt="<?php print t('Home') ?>" id="logo" />
+          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo" />
           </a>
         <?php endif; ?>
         
         <?php if ($site_name): ?>
           <h1 id='site-name'>
-            <a href="<?php print $base_path ?>" title="<?php print t('Home') ?>">
-              <?php print $site_name ?>
+            <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>">
+              <?php print $site_name; ?>
             </a>
           </h1>
         <?php endif; ?>
         
         <?php if ($site_slogan): ?> 
           <div id='site-slogan'>
-            <?php print $site_slogan ?>
+            <?php print $site_slogan; ?>
           </div>
         <?php endif; ?>
+        
       </div>
      
       
       <div class="menu <?php if ($primary_links) { print "withprimary"; } if ($secondary_links) { print " withsecondary"; } ?> ">
           <?php if ($primary_links): ?>
             <div id="primary" class="clear-block">
-              <?php print theme('menu_links', $primary_links) ?>
+              <?php print theme('menu_links', $primary_links); ?>
             </div>
           <?php endif; ?>
           
           <?php if ($secondary_links): ?>
             <div id="secondary" class="clear-block">
-              <?php print theme('menu_links', $secondary_links) ?>
+              <?php print theme('menu_links', $secondary_links); ?>
             </div>
           <?php endif; ?>
       </div>
       
-      <?php print $breadcrumb ?>
+      <?php print $breadcrumb; ?>
       
       <?php if ($header): ?>
         <div id="header-region">
-          <?php print $header ?>
+          <?php print $header; ?>
         </div>
       <?php endif; ?>
       
@@ -64,36 +65,36 @@
       
       <?php if ($sidebar_left): ?>
         <div id="sidebar-left" class="column sidebar">
-          <?php print $sidebar_left ?>
+          <?php print $sidebar_left; ?>
         </div>
       <?php endif; ?>  
 
       <?php if ($sidebar_right): ?>
         <div id="sidebar-right" class="column sidebar">
-          <?php print $sidebar_right ?>
+          <?php print $sidebar_right; ?>
         </div>
       <?php endif; ?>
 
       <div id="main" class="column clear-block">
-        <?php if ($mission): ?><div id="mission"><?php print $mission ?></div><?php endif; ?>
-        <?php if ($content_top):?><div id="content-top"><?php print $content_top ?></div><?php endif; ?>
-        <?php if ($title): ?><h1 class="title"><?php print $title ?></h1><?php endif; ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print $tabs ?></div><?php endif; ?>
-        <?php print $help ?>
-        <?php print $messages ?>
-        <?php print $content ?>
-        <?php print $feed_icons ?>
-        <?php if ($content_bottom): ?><div id="content-bottom"><?php print $content_bottom ?></div><?php endif; ?>
+        <?php if ($mission): ?><div id="mission"><?php print $mission; ?></div><?php endif; ?>
+        <?php if ($content_top):?><div id="content-top"><?php print $content_top; ?></div><?php endif; ?>
+        <?php if ($title): ?><h1 class="title"><?php print $title; ?></h1><?php endif; ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print $tabs; ?></div><?php endif; ?>
+        <?php print $help; ?>
+        <?php print $messages; ?>
+        <?php print $content; ?>
+        <?php print $feed_icons; ?>
+        <?php if ($content_bottom): ?><div id="content-bottom"><?php print $content_bottom; ?></div><?php endif; ?>
       </div>
     </div>
 
     <div id="footer-wrapper">
       <div id="footer">
-        <?php print $footer_message ?>
+        <?php print $footer_message; ?>
       </div>
     </div>
     
-    <?php print $closure ?>
+    <?php print $closure; ?>
     
   </div>
 
