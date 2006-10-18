405b1c91cffa3ef52abb12c6c90b3345af7426f7
Farsheed <zirafa@10692.no-reply.drupal.org>
2006-10-18 07:46:04 +0000

    Enclosing text in t() so they can be properly translated.

--- comment.tpl.php
+++ comment.tpl.php
@@ -2,7 +2,7 @@
 <?php if ($new != '') { ?><span class="new"><?php print $new; ?></span><?php } ?>
 <h3 class="title"><?php print $title; ?></h3>
 <?php if ($picture) print $picture; ?>
-    <div class="submitted">On <?php print format_date($comment->timestamp, 'custom', 'F jS, Y'); ?> <?php print theme('username', $comment); ?> says:</div>
+    <div class="submitted"><?php print t('On ') . format_date($comment->timestamp, 'custom', 'F jS, Y'); ?> <?php print theme('username', $comment) . t(' says:'); ?></div>
     <div class="content"><?php print $content; ?></div>
     <div class="links"><?php print $links; ?></div>
 </div>
