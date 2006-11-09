68d921b932f61d3fced7f200b0ba3ca03f09b69a
Jeff Robbins <jjeff@17190.no-reply.drupal.org>
2006-11-09 21:29:02 +0000

    removing blank line

--- block.tpl.php
+++ block.tpl.php
@@ -1,7 +1,6 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">  
   <div class="blockinner">
     <h2 class="title"> <?php print $block->subject; ?> </h2>
-
     <div class="content">
       <?php print $block->content; ?>
     </div>    
