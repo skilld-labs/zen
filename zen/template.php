6377d2edc62958216b714614575aa5094151e8b6
JohnAlbin <virtually.johnalbin@gmail.com>
2008-05-20 13:12:07 +0000

    #260016: PHP 4 Parse error: syntax error, unexpected '&'

--- zen/template.php
+++ zen/template.php
@@ -392,7 +392,9 @@ function zen_theme(&$existing, $type, $theme, $path) {
   // This function applies to every hook.
   $functions[0] = $theme . '_preprocess';
   // Inspect the preprocess functions for every hook in the theme registry.
-  foreach ($existing AS $hook => &$value) {
+  // @TODO: When PHP 5 becomes required (Zen 7.x), use the following faster
+  // implementation: foreach ($existing AS $hook => &$value) {}
+  foreach (array_keys($existing) AS $hook) {
     // Each theme has two possible preprocess functions that can act on a hook.
     // This function only applies to this hook.
     $functions[1] = $theme . '_preprocess_' . $hook;
