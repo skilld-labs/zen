d497dff7d1c55db0c52ab38942084fde1d5e540d
JohnAlbin <virtually.johnalbin@gmail.com>
2008-09-14 12:09:37 +0000

    #299397: Synchronize Zen's tpl files with core's tpl files; comment.tpl.php

--- zen/comment.tpl.php
+++ zen/comment.tpl.php
@@ -1,8 +1,46 @@
-<div class="comment <?php print $comment_classes; ?>"><div class="comment-inner">
+<?php
+// $Id$
+
+/**
+ * @file comment.tpl.php
+ * Default theme implementation for comments.
+ *
+ * Available variables:
+ * - $author: Comment author. Can be link or plain text.
+ * - $classes: A set of CSS classes for the DIV wrapping the comment.
+     Possible values are: comment, comment-new, comment-preview,
+     comment-unpublished, comment-published, odd, even, first, last,
+     comment-by-anon, comment-by-author, or comment-mine.
+ * - $content: Body of the post.
+ * - $date: Date and time of posting.
+ * - $links: Various operational links.
+ * - $new: New comment marker.
+ * - $picture: Authors picture.
+ * - $signature: Authors signature.
+ * - $status: Comment status. Possible values are:
+ *   comment-unpublished, comment-published or comment-preview.
+ * - $submitted: By line with date and time.
+ * - $title: Linked title.
+ * - $unpublished: Is the comment unpublished?
+ *
+ * These two variables are provided for context.
+ * - $comment: Full comment object.
+ * - $node: Node object the comments are attached to.
+ *
+ * @see template_preprocess_comment()
+ * @see theme_comment()
+ */
+?>
+<div class="<?php print $classes; ?>"><div class="comment-inner clear-block">
 
   <?php if ($title): ?>
-    <h3 class="title"><?php print $title; if (!empty($new)): ?> <span class="new"><?php print $new; ?></span><?php endif; ?></h3>
-  <?php elseif (!empty($new)): ?>
+    <h3 class="title">
+      <?php print $title; ?>
+      <?php if ($comment->new): ?>
+        <span class="new"><?php print $new; ?></span>
+      <?php endif; ?>
+    </h3>
+  <?php elseif ($comment->new): ?>
     <div class="new"><?php print $new; ?></div>
   <?php endif; ?>
 
@@ -18,6 +56,11 @@
 
   <div class="content">
     <?php print $content; ?>
+    <?php if ($signature): ?>
+    <div class="user-signature clear-block">
+      <?php print $signature; ?>
+    </div>
+    <?php endif; ?>
   </div>
 
   <?php if ($links): ?>
