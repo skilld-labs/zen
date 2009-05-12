20bd52be39963cb6893f24572bd7ce21a1cdfb95
JohnAlbin <virtually.johnalbin@gmail.com>
2009-05-12 18:39:50 +0000

    Changed "or" back to "||" in node.tpl.php since themers do need to learn a little PHP

--- zen/node.tpl.php
+++ zen/node.tpl.php
@@ -62,7 +62,7 @@
     <div class="unpublished"><?php print t('Unpublished'); ?></div>
   <?php endif; ?>
 
-  <?php if ($submitted or $terms): ?>
+  <?php if ($submitted || $terms): ?>
     <div class="meta">
       <?php if ($submitted): ?>
         <div class="submitted">
