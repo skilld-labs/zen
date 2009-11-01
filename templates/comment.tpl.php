338f95c6b5a16a3cc7b7e70983057100e49f159b
JohnAlbin <virtually.johnalbin@gmail.com>
2009-11-01 20:13:56 +0000

    Updated comment.tpl comments.

--- templates/comment.tpl.php
+++ templates/comment.tpl.php
@@ -2,32 +2,53 @@
 // $Id$
 
 /**
- * @file comment.tpl.php
+ * @file
  * Default theme implementation for comments.
  *
  * Available variables:
  * - $author: Comment author. Can be link or plain text.
- * - $classes: A set of CSS classes for the DIV wrapping the comment.
-     Possible values are: comment, comment-new, comment-preview,
-     comment-unpublished, comment-published, odd, even, first, last,
-     comment-by-anon, comment-by-author, or comment-mine.
  * - $content: Body of the post.
- * - $date: Date and time of posting.
- * - $links: Various operational links.
  * - $new: New comment marker.
+ * - $links: Various operational links.
  * - $picture: Authors picture.
  * - $signature: Authors signature.
  * - $status: Comment status. Possible values are:
  *   comment-unpublished, comment-published or comment-preview.
- * - $submitted: By line with date and time.
  * - $title: Linked title.
  * - $unpublished: Is the comment unpublished?
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the following:
+ *   - comment: The current template type, i.e., "theming hook".
+ *   - comment-by-anon: Comment by an unregistered user.
+ *   - comment-by-author: Comment by the author of the parent node.
+ *   - comment-preview: When previewing a new or edited comment.
+ *   - odd
+ *   - even
+ *   - first
+ *   - last
+ *   The following applies only to viewers who are registered users:
+ *   - comment-mine: Comment by the user currently viewing the page.
+ *   - comment-published
+ *   - comment-unpublished: An unpublished comment visible only to administrators.
+ *   - comment-new: New comment since the last visit.
  *
- * These two variables are provided for context.
+ * These two variables are provided for context:
  * - $comment: Full comment object.
  * - $node: Node object the comments are attached to.
  *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * The following variables are deprecated in Drupal 7:
+ * - $date: Date and time of posting.
+ * - $submitted: By line with date and time.
+ *
+ * @see template_preprocess()
  * @see template_preprocess_comment()
+ * @see zen_preprocess()
+ * @see zen_process()
  * @see theme_comment()
  */
 ?>
@@ -57,9 +78,9 @@
   <div class="content">
     <?php print $content; ?>
     <?php if ($signature): ?>
-    <div class="user-signature clearfix">
-      <?php print $signature; ?>
-    </div>
+      <div class="user-signature clearfix">
+        <?php print $signature; ?>
+      </div>
     <?php endif; ?>
   </div>
 
