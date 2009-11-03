66df2e50381b13ec2405e3001ed560009e85e5fb
JohnAlbin <virtually.johnalbin@gmail.com>
2009-11-03 19:47:19 +0000

    Added <span> around site name in page.tpl.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -128,15 +128,11 @@
           <?php if ($site_name): ?>
             <?php if ($title): ?>
               <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                <?php print $site_name; ?>
-                </a>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
               </strong></div>
             <?php else: /* Use h1 when the content title is empty */ ?>
               <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                <?php print $site_name; ?>
-                </a>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
               </h1>
             <?php endif; ?>
           <?php endif; ?>
