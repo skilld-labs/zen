3f52d8e7301b28c68fe716e5c4ce9a76eaac6861
JohnAlbin <virtually.johnalbin@gmail.com>
2010-01-21 13:38:57 +0000

    Synced comment-wrapper.tpl with D7.

--- templates/comment-wrapper.tpl.php
+++ templates/comment-wrapper.tpl.php
@@ -6,8 +6,9 @@
  * Default theme implementation to wrap comments.
  *
  * Available variables:
- * - $content: All comments for a given page. Also contains sorting controls
- *   and comment forms if the site is configured for it.
+ * - $content: The array of content-related elements for the node. Use
+ *   render($content) to print them all, or print a subset such as
+ *   render($content['comment_form']).
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
  *   preprocess functions. The default value has the following:
@@ -18,18 +19,8 @@
  * The constants below the variables show the possible values and should be
  * used for comparison.
  * - $display_mode
- *   - COMMENT_MODE_FLAT_COLLAPSED
- *   - COMMENT_MODE_FLAT_EXPANDED
- *   - COMMENT_MODE_THREADED_COLLAPSED
- *   - COMMENT_MODE_THREADED_EXPANDED
- * - $display_order
- *   - COMMENT_ORDER_NEWEST_FIRST
- *   - COMMENT_ORDER_OLDEST_FIRST
- * - $comment_controls_state
- *   - COMMENT_CONTROLS_ABOVE
- *   - COMMENT_CONTROLS_BELOW
- *   - COMMENT_CONTROLS_ABOVE_BELOW
- *   - COMMENT_CONTROLS_HIDDEN
+ *   - COMMENT_MODE_FLAT
+ *   - COMMENT_MODE_THREADED
  *
  * Other variables:
  * - $classes_array: Array of html class attribute values. It is flattened
@@ -38,10 +29,15 @@
  * @see template_preprocess_comment_wrapper()
  */
 ?>
-<div id="comments" class="<?php print $classes; ?>">
+<div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($node->type != 'forum'): ?>
     <h2 class="title"><?php print t('Comments'); ?></h2>
   <?php endif; ?>
 
-  <?php print $content; ?>
+  <?php print render($content['comments']); ?>
+
+  <?php if ($content['comment_form']): ?>
+    <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+    <?php print render($content['comment_form']); ?>
+  <?php endif; ?>
 </div>
