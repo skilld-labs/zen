c37ef43290b7c824dcbfff3f1cf8254caa8a7372
JohnAlbin <virtually.johnalbin@gmail.com>
2011-01-29 16:18:24 +0000

    #1044586 by JohnAlbin: Fix "Rebuild theme registry" feature

--- template.php
+++ template.php
@@ -13,7 +13,10 @@
 
 // Auto-rebuild the theme registry during theme development.
 if (theme_get_setting('zen_rebuild_registry')) {
+  // Rebuild .info data.
   system_rebuild_theme_data();
+  // Rebuild theme registry.
+  drupal_theme_rebuild();
 }
 
 
