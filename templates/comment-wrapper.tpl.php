a6f5f8fc865d3a19cc3c19de73148d6ddb048197
JohnAlbin <virtually.johnalbin@gmail.com>
2011-02-19 03:28:55 +0000

    #1066472 by JohnAlbin: Render comments and form to see if headings are needed

--- templates/comment-wrapper.tpl.php
+++ templates/comment-wrapper.tpl.php
@@ -26,16 +26,20 @@
  *
  * @see template_preprocess_comment_wrapper()
  */
+
+// Render the comments and form first to see if we need headings.
+$comments = render($content['comments']);
+$comment_form = render($content['comment_form']);
 ?>
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($node->type != 'forum'): ?>
+  <?php if ($comments && $node->type != 'forum'): ?>
     <h2 class="title"><?php print t('Comments'); ?></h2>
   <?php endif; ?>
 
-  <?php print render($content['comments']); ?>
+  <?php print $comments; ?>
 
-  <?php if ($content['comment_form']): ?>
+  <?php if ($comment_form): ?>
     <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
-    <?php print render($content['comment_form']); ?>
+    <?php print $comment_form; ?>
   <?php endif; ?>
 </div>
