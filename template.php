9d330c3b9367787375529d39fdb54dbaf2cbdebb
JohnAlbin <virtually.johnalbin@gmail.com>
2011-03-31 15:19:59 +0800

    Issue #1084504 by Crossfeed, komal.savla, Network and JohnAlbin: PHP Error when secondary menu or menu module is disabled

--- template.php
+++ template.php
@@ -223,8 +223,13 @@ function zen_preprocess_html(&$variables, $hook) {
 function zen_preprocess_page(&$variables, $hook) {
   // Find the title of the menu used by the secondary links.
   $secondary_links = variable_get('menu_secondary_links_source', 'user-menu');
-  $menus = menu_get_menus();
-  $variables['secondary_menu_heading'] = $menus[$secondary_links];
+  if ($secondary_links) {
+    $menus = function_exists('menu_get_menus') ? menu_get_menus() : menu_list_system_menus();
+    $variables['secondary_menu_heading'] = $menus[$secondary_links];
+  }
+  else {
+    $variables['secondary_menu_heading'] = '';
+  }
 }
 
 /**
