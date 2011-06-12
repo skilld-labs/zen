abb4efe9b2ca35781ec021ee8b26bf2945b37f62
JohnAlbin <virtually.johnalbin@gmail.com>
2011-06-12 21:24:09 +0800

    Issue #535038 by JohnAlbin: Convert to HTML 5
    Simplify script and style tags.

--- template.php
+++ template.php
@@ -218,6 +218,23 @@ function zen_preprocess_html(&$variables, $hook) {
 }
 
 /**
+ * Override or insert variables in the html_tag theme function.
+ */
+function zen_process_html_tag(&$variables) {
+  $tag = &$variables['element'];
+
+  if ($tag['#tag'] == 'style' || $tag['#tag'] == 'script') {
+    // Remove redundant type attribute and CDATA comments.
+    unset($tag['#attributes']['type'], $tag['#value_prefix'], $tag['#value_suffix']);
+
+    // Remove media="all" but leave others unaffected.
+    if (isset($tag['#attributes']['media']) && $tag['#attributes']['media'] === 'all') {
+      unset($tag['#attributes']['media']);
+    }
+  }
+}
+
+/**
  * Implement hook_html_head_alter().
  */
 function zen_html_head_alter(&$head) {
