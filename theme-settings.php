4e93915bf51ec845860d7c026002332589c1ae71
JohnAlbin <virtually.johnalbin@gmail.com>
2011-09-28 05:31:16 +0800

    by JohnAlbin: Work-around for a core bug affecting admin themes. See issue #943212.

--- theme-settings.php
+++ theme-settings.php
@@ -7,10 +7,13 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function zen_form_system_theme_settings_alter(&$form, $form_state) {
-  /*
-   * Create the form using Forms API
-   */
+function zen_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
+  }
+
+  // Create the form using Forms API
   $form['breadcrumb'] = array(
     '#type'          => 'fieldset',
     '#title'         => t('Breadcrumb settings'),
