99dbd6689806189a2c10e902c66f252e327200f5
JohnAlbin <virtually.johnalbin@gmail.com>
2011-11-17 04:47:48 +0800

    Issue #1343896 by JohnAlbin: Convert comment markup to HTML5

--- templates/comment.tpl.php
+++ templates/comment.tpl.php
@@ -63,42 +63,44 @@
  * @see theme_comment()
  */
 ?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <h3<?php print $title_attributes; ?>>
-      <?php print $title; ?>
-      <?php if ($new): ?>
-        <span class="new"><?php print $new; ?></span>
-      <?php endif; ?>
-    </h3>
-  <?php elseif ($new): ?>
-    <div class="new"><?php print $new; ?></div>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <header>
+    <p class="submitted">
+      <?php print $picture; ?>
+      <?php print $submitted; ?>
+      <?php print $permalink; ?>
+    </p>
 
-  <?php if ($status == 'comment-unpublished'): ?>
-    <div class="unpublished"><?php print t('Unpublished'); ?></div>
-  <?php endif; ?>
-
-  <div class="submitted">
-    <?php print $picture; ?>
-    <?php print $submitted; ?>
-    <?php print $permalink; ?>
-  </div>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['links']);
-      print render($content);
-    ?>
-    <?php if ($signature): ?>
-      <div class="user-signature clearfix">
-        <?php print $signature; ?>
-      </div>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h3<?php print $title_attributes; ?>>
+        <?php print $title; ?>
+        <?php if ($new): ?>
+          <mark class="new"><?php print $new; ?></mark>
+        <?php endif; ?>
+      </h3>
+    <?php elseif ($new): ?>
+      <mark class="new"><?php print $new; ?></mark>
     <?php endif; ?>
-  </div>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($status == 'comment-unpublished'): ?>
+      <p class="unpublished"><?php print t('Unpublished'); ?></p>
+    <?php endif; ?>
+
+  </header>
+
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php if ($signature): ?>
+    <footer class="user-signature clearfix">
+      <?php print $signature; ?>
+    </footer>
+  <?php endif; ?>
 
   <?php print render($content['links']) ?>
-</div><!-- /.comment -->
+</article><!-- /.comment -->
