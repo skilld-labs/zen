0f378c74300920916efde65c9d1018eb63a70f42
JohnAlbin <virtually.johnalbin@gmail.com>
2011-11-17 05:16:23 +0800

    Issue #1343982 by JohnAlbin: Convert node markup to HTML5

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -83,36 +83,36 @@
  * @see template_process()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && $title): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($unpublished): ?>
-    <div class="unpublished"><?php print t('Unpublished'); ?></div>
-  <?php endif; ?>
+    <?php if ($display_submitted): ?>
+      <p class="submitted">
+        <?php print $user_picture; ?>
+        <?php print $submitted; ?>
+      </p>
+    <?php endif; ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($unpublished): ?>
+      <p class="unpublished"><?php print t('Unpublished'); ?></p>
+    <?php endif; ?>
+  </header>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+  ?>
 
   <?php print render($content['links']); ?>
 
   <?php print render($content['comments']); ?>
 
-</div><!-- /.node -->
+</article><!-- /.node -->
