a168f4c919e2bac38b6689724b5951f66dd989c2
echoz <git@38665.no-reply.drupal.org>
2011-11-25 13:52:43 +0800

    In page.tpl, add header tag and add hgroup around site name and slogan

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -72,30 +72,24 @@
 
 <div id="page-wrapper"><div id="page">
 
-  <div id="header"><div class="section clearfix">
+  <header id="header"><div class="section clearfix">
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
+      <hgroup id="name-and-slogan">
         <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong></div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+          <h1 id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
         <?php endif; ?>
 
         <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </div><!-- /#name-and-slogan -->
+      </hgroup><!-- /#name-and-slogan -->
     <?php endif; ?>
 
     <?php print theme('links__system_secondary_menu', array(
@@ -113,7 +107,7 @@
 
     <?php print render($page['header']); ?>
 
-  </div></div><!-- /.section, /#header -->
+  </div></header><!-- /.section, /#header -->
 
   <div id="main-wrapper"><div id="main" class="clearfix">
 
