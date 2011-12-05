5e7d645c5832ff72f3a8a9aab3e2203eaaab8985
JohnAlbin <virtually.johnalbin@gmail.com>
2011-12-05 23:33:41 +0800

    Fix paths in generate.sh

--- zen-internals/css/generate.sh
+++ zen-internals/css/generate.sh
@@ -5,7 +5,7 @@
 
 rm *.css;
 for FILENAME in ../../STARTERKIT/css/*.css; do
-  cp ../../STARTERKIT/css/$FILENAME .;
+  cp $FILENAME .;
 done
 
 # Don't need the core reference.
@@ -13,5 +13,5 @@ rm drupal7-reference.css;
 
 rm ../images/*;
 for FILENAME in ../../STARTERKIT/images/*; do
-  cp ../../STARTERKIT/css/$FILENAME ../images/;
+  cp $FILENAME ../images/;
 done
