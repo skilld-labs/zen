a8344076c27ca51533786066e69191703ef6dc33
JohnAlbin <virtually.johnalbin@gmail.com>
2012-04-01 21:24:22 +0800

    Issue #1422560: Add note about Menu Block module next to menu variables in page.tpl

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -137,7 +137,12 @@
 
       <?php if ($main_menu): ?>
         <nav id="main-menu" role="navigation">
-          <?php print theme('links__system_main_menu', array(
+          <?php
+          // This code snippet is hard to modify. We recommend turning off the
+          // "Main menu" on your sub-theme's settings form, deleting this PHP
+          // code block, and, instead, using the "Menu block" module.
+          // @see http://drupal.org/project/menu_block
+          print theme('links__system_main_menu', array(
             'links' => $main_menu,
             'attributes' => array(
               'class' => array('links', 'inline', 'clearfix'),
