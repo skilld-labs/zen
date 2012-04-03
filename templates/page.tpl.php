f377f4cc2b5759d23dc63b89fa2a9a8fc415375c
JohnAlbin <virtually.johnalbin@gmail.com>
2012-04-03 13:56:43 +0800

    Remove clearfix classes from page.tpl.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -72,7 +72,7 @@
 
 <div id="page">
 
-  <header id="header" class="clearfix" role="banner">
+  <header id="header" role="banner">
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
@@ -112,7 +112,7 @@
 
   </header>
 
-  <div id="main" class="clearfix">
+  <div id="main">
 
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
@@ -133,7 +133,7 @@
       <?php print $feed_icons; ?>
     </div><!-- /#content -->
 
-    <div id="navigation" class="clearfix">
+    <div id="navigation">
 
       <?php if ($main_menu): ?>
         <nav id="main-menu" role="navigation">
