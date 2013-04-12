98cb1c77b9993f09d4d25a303a90eea28d465996
echoz <echoz@38665.no-reply.drupal.org>
2013-04-11 18:56:25 -0700

    Issue #1959000 by rupl and echoz: Remove hgroup because it's being dropped from the HTML5 spec.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -17,7 +17,7 @@
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
-      <hgroup class="header--name-and-slogan" id="name-and-slogan">
+      <div class="header--name-and-slogan" id="name-and-slogan">
         <?php if ($site_name): ?>
           <h1 class="header--site-name" id="site-name">
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header--site-link" rel="home"><span><?php print $site_name; ?></span></a>
@@ -27,7 +27,7 @@
         <?php if ($site_slogan): ?>
           <h2 class="header--site-slogan" id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </hgroup>
+      </div>
     <?php endif; ?>
 
     <?php if ($secondary_menu): ?>
