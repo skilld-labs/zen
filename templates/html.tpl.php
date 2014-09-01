627e11fb363739564988401531c9b37b2a106a48
JohnAlbin <virtually.johnalbin@gmail.com>
2014-09-02 07:18:05 +0800

    Re-add initial-scale=1.0 for testing.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -20,7 +20,7 @@
   <?php if ($default_mobile_metatags): ?>
     <meta name="MobileOptimized" content="width">
     <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php endif; ?>
   <meta http-equiv="cleartype" content="on">
 
