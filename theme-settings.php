ecdca63f0a3bdffe4a1889889aecc6eb3369e7d0
JohnAlbin <virtually.johnalbin@gmail.com>
2015-06-28 12:05:58 +0800

    Fix lint errors.

--- theme-settings.php
+++ theme-settings.php
@@ -1,11 +1,6 @@
 <?php
 /**
  * Implements hook_form_system_theme_settings_alter() function.
- *
- * @param $form
- *   Nested array of form elements that comprise the form.
- * @param $form_state
- *   A keyed array containing the current state of the form.
  */
 function zen_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
