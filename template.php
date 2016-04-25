0797d87aa7971bb32978d9170a81f1916d0604b1
JohnAlbin <virtually.johnalbin@gmail.com>
2016-04-25 22:19:14 +0800

    Add zen.theme file.

--- template.php
+++ template.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * @file
- * Contains functions to alter Drupal's markup for the Zen theme.
- *
- * IMPORTANT WARNING: DO NOT MODIFY THIS FILE.
- *
- * The base Zen theme is designed to be easily extended by its sub-themes. You
- * shouldn't modify this or any of the CSS or PHP files in the root zen/ folder.
- * See the online documentation for more information:
- *   https://drupal.org/documentation/theme/zen
- */
 
 // Auto-rebuild the theme registry during theme development.
 if (theme_get_setting('zen_rebuild_registry') && !defined('MAINTENANCE_MODE')) {
