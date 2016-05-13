c2c9b64fdfe8af89ba4b4f47d248542357ffcc1a
JohnAlbin <virtually.johnalbin@gmail.com>
2016-05-13 17:35:03 -0500

    Issue #2724831 by JohnAlbin: Convert breadcrumbs component to be Drupal 8-compatible

--- theme-settings.php
+++ theme-settings.php
@@ -8,60 +8,6 @@ function zen_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStat
     return;
   }
 
-  // Create the form using Forms API
-  $form['breadcrumb'] = array(
-    '#type'          => 'fieldset',
-    '#title'         => t('Breadcrumb settings'),
-  );
-  $form['breadcrumb']['zen_breadcrumb'] = array(
-    '#type'          => 'select',
-    '#title'         => t('Display breadcrumb'),
-    '#default_value' => theme_get_setting('zen_breadcrumb'),
-    '#options'       => array(
-                          'yes'   => t('Yes'),
-                          'admin' => t('Only in admin section'),
-                          'no'    => t('No'),
-                        ),
-  );
-  $form['breadcrumb']['breadcrumb_options'] = array(
-    '#type' => 'container',
-    '#states' => array(
-      'invisible' => array(
-        ':input[name="zen_breadcrumb"]' => array('value' => 'no'),
-      ),
-    ),
-  );
-  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_separator'] = array(
-    '#type'          => 'textfield',
-    '#title'         => t('Breadcrumb separator'),
-    '#description'   => t('Text only. Don’t forget to include spaces.'),
-    '#default_value' => theme_get_setting('zen_breadcrumb_separator'),
-    '#size'          => 5,
-    '#maxlength'     => 10,
-  );
-  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_home'] = array(
-    '#type'          => 'checkbox',
-    '#title'         => t('Show home page link in breadcrumb'),
-    '#default_value' => theme_get_setting('zen_breadcrumb_home'),
-  );
-  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_trailing'] = array(
-    '#type'          => 'checkbox',
-    '#title'         => t('Append a separator to the end of the breadcrumb'),
-    '#default_value' => theme_get_setting('zen_breadcrumb_trailing'),
-    '#description'   => t('Useful when the breadcrumb is placed just before the title.'),
-    '#states' => array(
-      'disabled' => array(
-        ':input[name="zen_breadcrumb_title"]' => array('checked' => TRUE),
-      ),
-    ),
-  );
-  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_title'] = array(
-    '#type'          => 'checkbox',
-    '#title'         => t('Append the content title to the end of the breadcrumb'),
-    '#default_value' => theme_get_setting('zen_breadcrumb_title'),
-    '#description'   => t('Useful when the breadcrumb is not placed just before the title.'),
-  );
-
   $form['support'] = array(
     '#type'          => 'fieldset',
     '#title'         => t('Accessibility and support settings'),
