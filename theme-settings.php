d244a8e7ce368c3a44a2a2ca254dc72ed6c45252
JohnAlbin <virtually.johnalbin@gmail.com>
2016-05-24 17:39:43 +0800

    Revert "Issue #2724831 by JohnAlbin: Convert breadcrumbs component to be Drupal 8-compatible"
    This reverts commit c2c9b64fdfe8af89ba4b4f47d248542357ffcc1a.

--- theme-settings.php
+++ theme-settings.php
@@ -8,6 +8,78 @@ function zen_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStat
     return;
   }
 
+  // Create the form using Forms API
+  $form['breadcrumb'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('Breadcrumb settings'),
+  );
+  $form['breadcrumb']['zen_breadcrumb'] = array(
+    '#type'          => 'select',
+    '#title'         => t('Display breadcrumb'),
+    '#default_value' => theme_get_setting('zen_breadcrumb'),
+    '#options'       => array(
+                          'yes'   => t('Yes'),
+                          'admin' => t('Only in admin section'),
+                          'no'    => t('No'),
+                        ),
+  );
+  $form['breadcrumb']['breadcrumb_options'] = array(
+    '#type' => 'container',
+    '#states' => array(
+      'invisible' => array(
+        ':input[name="zen_breadcrumb"]' => array('value' => 'no'),
+      ),
+    ),
+  );
+  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_separator'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Breadcrumb separator'),
+    '#description'   => t('Text only. Don’t forget to include spaces.'),
+    '#default_value' => theme_get_setting('zen_breadcrumb_separator'),
+    '#size'          => 5,
+    '#maxlength'     => 10,
+  );
+  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_home'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Show home page link in breadcrumb'),
+    '#default_value' => theme_get_setting('zen_breadcrumb_home'),
+  );
+  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_trailing'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Append a separator to the end of the breadcrumb'),
+    '#default_value' => theme_get_setting('zen_breadcrumb_trailing'),
+    '#description'   => t('Useful when the breadcrumb is placed just before the title.'),
+    '#states' => array(
+      'disabled' => array(
+        ':input[name="zen_breadcrumb_title"]' => array('checked' => TRUE),
+      ),
+    ),
+  );
+  $form['breadcrumb']['breadcrumb_options']['zen_breadcrumb_title'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Append the content title to the end of the breadcrumb'),
+    '#default_value' => theme_get_setting('zen_breadcrumb_title'),
+    '#description'   => t('Useful when the breadcrumb is not placed just before the title.'),
+  );
+
+  $form['support'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('Accessibility and support settings'),
+  );
+  $form['support']['zen_skip_link_anchor'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Anchor ID for the “skip link”'),
+    '#default_value' => theme_get_setting('zen_skip_link_anchor'),
+    '#field_prefix'  => '#',
+    '#description'   => t('Specify the HTML ID of the element that the accessible-but-hidden “skip link” should link to. Note: that element should have the <code>tabindex="-1"</code> attribute to prevent an accessibility bug in webkit browsers. (<a href="!link">Read more about skip links</a>.)', array('!link' => 'https://drupal.org/node/467976')),
+  );
+  $form['support']['zen_skip_link_text'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Text for the “skip link”'),
+    '#default_value' => theme_get_setting('zen_skip_link_text'),
+    '#description'   => t('For example: <em>Jump to navigation</em>, <em>Skip to content</em>'),
+  );
+
   $form['themedev'] = array(
     '#type'          => 'fieldset',
     '#title'         => t('Theme development settings'),
