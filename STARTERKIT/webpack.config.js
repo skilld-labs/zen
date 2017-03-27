05e2731876f0ddf831f96f7d3a68a7455488f05e
Gretel Trost <gretel.trost@jadelabs.net>
2017-03-27 17:10:52 -0300

    Adding Webpack v2 and Babel for ES6 Support

---
+++ STARTERKIT/webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+
+var config = {
+  context: __dirname + '/js',
+  entry: {
+    app: './init.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  plugins: [
+      new webpack.DefinePlugin({
+          'process.env': {
+              'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+          }
+      }),
+  ]
+};
+
+process.noDeprecation = true;
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true
+            }
+        })
+    )
+}
+
+module.exports = config;
