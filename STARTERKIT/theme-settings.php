fe504efc244471b259ab9b365309e850ecd74285
andriyun <andriy.yun@gmail.com>
2017-04-28 19:09:32 +0300

    Removing Zen dependencies for starterkit

--- STARTERKIT/theme-settings.php
+++ STARTERKIT/theme-settings.php
@@ -13,21 +13,14 @@ function STARTERKIT_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\F
     return;
   }
 
-  // Create the form using Forms API: http://api.drupal.org/api/7
-
-  /* -- Delete this line if you want to use this setting
-  $form['STARTERKIT_example'] = array(
-  '#type'          => 'checkbox',
-  '#title'         => t('STARTERKIT sample setting'),
-  '#default_value' => theme_get_setting('STARTERKIT_example'),
-  '#description'   => t("This example option doesn't do anything."),
+  $form['themedev'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('Theme development settings'),
   );
-  // */
-
-  /* -- Delete this line if you want to remove this base theme setting.
-  // We don't need breadcrumbs to be configurable on this site.
-  unset($form['breadcrumb']);
-  // */
-
-  // We are editing the $form in place, so we don't need to return anything.
+  $form['themedev']['zen_rebuild_registry'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Rebuild theme registry and output template debugging on every page.'),
+    '#default_value' => theme_get_setting('zen_rebuild_registry'),
+    '#description'   => t('During theme development, it can be very useful to continuously <a href="!link">rebuild the theme registry</a> and to output template debugging HTML comments. WARNING: this is a huge performance penalty and must be turned off on production websites.', array('!link' => 'https://drupal.org/node/173880#theme-registry')),
+  );
 }
